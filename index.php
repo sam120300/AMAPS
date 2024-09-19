73227099a45b6dc13f7f9bf1c3c3e45f414ef983
Sam120300 <shingsam03@gmail.com>
2024-09-19 23:26:16 +0800

    19-09-2024

--- index.php
+++ index.php
@@ -367,7 +367,7 @@ if(isset($_POST['login'])){
 <body>
     <div class="video-container">
         <video autoplay muted loop>
-            <source src="assets/bg.mp4" type="video/mp4">
+            <source src="assets/eyes_bg.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
     </div>  
